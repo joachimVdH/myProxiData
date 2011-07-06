6ff30f3de2118201413f611566716f7851b5aa71
Joachim Van der Hoeven <JoachimVdH@gmail.com>
2011-07-06 21:49:27 +0200

    repaired the corrupted file from during the merge

--- myProxiDataLogicTests/myProxiDataLogicTests.m
+++ myProxiDataLogicTests/myProxiDataLogicTests.m
@@ -11,30 +11,26 @@
 
 @implementation myProxiDataLogicTests
 /*
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-    // Run before each test method
+ - (void)setUp
+ {
+ [super setUp];
+ 
+ // Set-up code here.
+ // Run before each test method
+ 
+ }
+ 
+ - (void)tearDown
+ {
+ // Tear-down code here.
+ // Run after each test method
+ 
+ [super tearDown];
+ }
+ */
 
-}
-
-- (void)tearDown
-{
-  // Tear-down code here.
-	// Run after each test method
-    
-    [super tearDown];
-}
-*/
-
-<<<<<<< HEAD
-- (void)setUpClass {
-=======
 - (void)setUp {
   [super setUp];
->>>>>>> f890e7bc6b1d81a6e081a1b428d92b226da272cd
 	// Run at start of all tests in the class
 	NSString *fullPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"resultPageNL_110421" ofType:@"txt"];
 	htmlNL = [NSData dataWithContentsOfFile:fullPath];
@@ -42,34 +38,22 @@
   //[proximus parseData:htmlNL];
 }
 
-<<<<<<< HEAD
-- (void)tearDownClass {
-	// Run at end of all tests in the class	
-	[htmlNL release];
-	[proximus release];
-=======
 - (void)tearDown {
   [super tearDown];
 	// Run at end of all tests in the class	
-//	[htmlNL release];
-//	[proximus release];
->>>>>>> f890e7bc6b1d81a6e081a1b428d92b226da272cd
+  //	[htmlNL release];
+  //	[proximus release];
 }
 
 /*
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in myProxiDataLogicTests");
-}
-*/
+ - (void)testExample
+ {
+ STFail(@"Unit tests are not implemented yet in myProxiDataLogicTests");
+ }
+ */
 
 - (void) testInitializesWithHTMLData
 {
-<<<<<<< HEAD
-  STAssertNil(htmlNL, @"file is loaded");
-  STAssertNil(proximus, @"Proximus initialized");
-}
-=======
   //STAssertNil(htmlNL, @"file is loaded");
   //STAssertNil(proximus, @"Proximus initialized");
   STAssertNotNil(htmlNL, @"file is not loaded");
@@ -95,5 +79,4 @@
 
 
 
->>>>>>> f890e7bc6b1d81a6e081a1b428d92b226da272cd
 @end
